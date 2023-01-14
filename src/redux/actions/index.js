313433de07b9725649ce4f133fcca3918d9f3900
Mhd Rizki Purba <5422573-muhammadrizkipurba@users.noreply.gitlab.com>
2023-01-14 19:37:55 +0700

    use cors-anywhere herokuapp

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,7 @@ import { RESET_SEARCH_HISTORY, SET_SEARCH_HISTORY, SET_SELECTED_PLACE } from "..
 
 export const getPlaceDetails = (placeId) => async (dispatch) => {
   const response = await axios.get(
-    `/maps/api/place/details/json?place_id=${placeId}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+    `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
   );
   return response.data;
 };
